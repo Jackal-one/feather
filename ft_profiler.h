7a730e182a8efa4514955e0d34a8092d69ef710d
Jackal-one <michelepa87@gmail.com>
2018-10-19 22:55:23 +0200

    fixed profiler static initialization + self instrumentation!

--- ft_profiler.h
+++ ft_profiler.h
@@ -10,10 +10,15 @@ const uint32_t k_max_name_len = 32u;
 const uint32_t k_max_tokens = 1024u;
 const uint32_t k_max_threads = 16u;
 
+#define FT_TIMER 0u
+#define FT_EVENT 1u
+#define FT_COUNTER 2u
+
 #define FT_TOKEN_PASTE(a, b) a ## b
 #define FT_TOKEN_PASTE_EX(a, b) FT_TOKEN_PASTE(a, b)
-#define FT_SCOPE(group, name) \
-  static uint64_t FT_TOKEN_PASTE_EX(s_token, __LINE__) = ft_make_token(group, name); \
+#define FT_DEFINE(token, group, name, event) uint64_t ft_token_##token = ft_make_token(group, name)
+#define FT_SCOPE_TOK(token) ft_scope_t FT_TOKEN_PASTE_EX(scope, __LINE__)(ft_token_##token)
+#define FT_SCOPE(group, name) static uint64_t FT_TOKEN_PASTE_EX(s_token, __LINE__) = ft_make_token(group, name); \
   ft_scope_t FT_TOKEN_PASTE_EX(scope, __LINE__)(FT_TOKEN_PASTE_EX(s_token, __LINE__))
 
 struct ft_profile_data_t {
@@ -109,7 +114,11 @@ struct ft_profiler_t {
 };
 
 thread_local ft_timeline_t* g_tls_timeline = nullptr;
-static ft_profiler_t* g_profiler = nullptr;
+
+FT_DEFINE(flush_data_internal, "feather", "ft_flush_data", FT_TIMER);
+FT_DEFINE(flush_data_callback, "feather", "callback", FT_TIMER);
+FT_DEFINE(flush_data_event, "feather", "ft_flush_data", FT_EVENT);
+FT_DEFINE(memory_used, "feather", "memory_used", FT_EVENT);
 
 uint32_t platform_clz(const uint32_t mask) {
   return __builtin_ctz(mask);
@@ -130,9 +139,14 @@ void ft_util_meta_strcpy(const uint32_t index, char* dest, const char* name) {
   std::memset(&dest[index * k_max_name_len + str_len], '\0', 1u);
 }
 
+ft_profiler_t* ft_profiler() {
+  static ft_profiler_t profiler_inst;
+  return &profiler_inst;
+}
+
 bool ft_find_token(uint64_t& token, const char* group, const char* name) {
-  for (uint32_t i=0u; i<g_profiler->tokens_meta.used; i++) {
-    if (std::strcmp(&g_profiler->tokens_meta.names[i * k_max_name_len], name) == 0) {
+  for (uint32_t i=0u; i<ft_profiler()->tokens_meta.used; i++) {
+    if (std::strcmp(&ft_profiler()->tokens_meta.names[i * k_max_name_len], name) == 0) {
       token = ft_pack_token(i);
       return true;
     }
@@ -142,13 +156,13 @@ bool ft_find_token(uint64_t& token, const char* group, const char* name) {
 }
 
 uint64_t ft_push_token(const char* group, const char* name) {
-  const uint32_t index = g_profiler->tokens_meta.used++;
-  ft_util_meta_strcpy(index, g_profiler->tokens_meta.names, name);
+  const uint32_t index = ft_profiler()->tokens_meta.used++;
+  ft_util_meta_strcpy(index, ft_profiler()->tokens_meta.names, name);
   return ft_pack_token(index);
 }
 
 uint64_t ft_make_token(const char* group, const char* name) {
-  std::lock_guard<std::mutex> lock(g_profiler->lock);
+  std::lock_guard<std::mutex> lock(ft_profiler()->lock);
   uint64_t token = 0u;
   if (!ft_find_token(token, group, name)) {
     token = ft_push_token(group, name);
@@ -159,7 +173,7 @@ uint64_t ft_make_token(const char* group, const char* name) {
 
 bool ft_find_next_free_index(uint32_t& index) {
   for (uint32_t dword_index=0u; dword_index<k_max_timeline_bitmasks; ++dword_index) {
-    uint32_t& bitmask = g_profiler->free_list[dword_index];
+    uint32_t& bitmask = ft_profiler()->free_list[dword_index];
     if (bitmask > 0u) {
       const uint32_t bit_index = platform_clz(bitmask);
       bitmask ^= 1u << bit_index;
@@ -174,11 +188,11 @@ bool ft_find_next_free_index(uint32_t& index) {
 void ft_release_index(const uint32_t index) {
   const uint32_t dword_index = index / 32u;
   const uint32_t bit_index = index % 32u;
-  g_profiler->free_list[dword_index] ^= 1u << bit_index;
+  ft_profiler()->free_list[dword_index] ^= 1u << bit_index;
 }
 
 void ft_reset_timeline(const uint32_t index) {
-  ft_timeline_t* tl = &g_profiler->timeline_pool[index];
+  ft_timeline_t* tl = &ft_profiler()->timeline_pool[index];
   tl->num_used = 0u;
   tl->thread_hash = 0u;
   tl->flags_release = 0u;
@@ -186,7 +200,7 @@ void ft_reset_timeline(const uint32_t index) {
 
 bool ft_find_thread_timeline(const uint64_t thread_hash, uint32_t& index) {
   for (uint32_t i=0u; i<k_max_threads; i++) {
-    if (g_profiler->timeline_pool[i].thread_hash == thread_hash) {
+    if (ft_profiler()->timeline_pool[i].thread_hash == thread_hash) {
       index = i; 
       return true;
     }
@@ -196,22 +210,22 @@ bool ft_find_thread_timeline(const uint64_t thread_hash, uint32_t& index) {
 }
 
 void ft_request_thread_timeline(const char* name) {
-  std::lock_guard<std::mutex> lock(g_profiler->lock);
+  std::lock_guard<std::mutex> lock(ft_profiler()->lock);
   const size_t thread_hash = std::hash<std::thread::id>{}(std::this_thread::get_id());
   uint32_t tl_index = -1;
   uint32_t free_index = -1;
 
   if (!ft_find_thread_timeline(thread_hash, tl_index)) {
     if (ft_find_next_free_index(free_index)) {
-      g_tls_timeline = &g_profiler->timeline_pool[free_index];
+      g_tls_timeline = &ft_profiler()->timeline_pool[free_index];
       g_tls_timeline->thread_hash = thread_hash;
-      ft_util_meta_strcpy(free_index, g_profiler->tokens_meta.thread_names, name ? name : "unknown");
+      ft_util_meta_strcpy(free_index, ft_profiler()->tokens_meta.thread_names, name ? name : "unknown");
     }
   }
 }
 
 void ft_release_thread_timeline() {
-  std::lock_guard<std::mutex> lock(g_profiler->lock);
+  std::lock_guard<std::mutex> lock(ft_profiler()->lock);
   g_tls_timeline->flags_release = 1u;
   g_tls_timeline = nullptr;
 }
@@ -232,7 +246,7 @@ uint64_t ft_pack_timestamp(uint32_t meta_index, uint64_t timestamp, uint8_t type
 
 void ft_write_qword(ft_timeline_t* timeline, uint64_t data) {
   const uint32_t next = timeline->num_used % k_max_timestamps;
-  uint64_t* buffer = (uint64_t*)&g_profiler->mem_arena[timeline->buffer_address];
+  uint64_t* buffer = (uint64_t*)&ft_profiler()->mem_arena[timeline->buffer_address];
   std::memcpy(&buffer[next], &data, sizeof(uint64_t));
   timeline->num_used.fetch_add(1u, std::memory_order_release);
 }
@@ -259,35 +273,39 @@ void ft_data_read(const ft_profile_data_t* data, const uint32_t thread_index,
 
   *ts = (qword & k_mask_value);
   *type = (qword >> (k_bits_value + k_bits_meta)) & k_mask_type;
-  *name = &g_profiler->tokens_meta.names[meta_index * k_max_name_len];
+  *name = &ft_profiler()->tokens_meta.names[meta_index * k_max_name_len];
 }
 
 void ft_flush_data(ft_callback flush_data, void* user_data) {
-  std::lock_guard<std::mutex> lock(g_profiler->lock);
-  g_profiler->profile_data.num_threads = 0u;
+  FT_SCOPE_TOK(flush_data_internal);
+  std::lock_guard<std::mutex> lock(ft_profiler()->lock);
+  ft_profiler()->profile_data.num_threads = 0u;
 
   for (uint32_t i=0u; i<k_max_threads; ++i) {
-    const ft_timeline_t* timeline = &g_profiler->timeline_pool[i];
+    const ft_timeline_t* timeline = &ft_profiler()->timeline_pool[i];
 
     if (timeline->thread_hash > 0u) {
       const uint32_t num_used = timeline->num_used.load(std::memory_order_acquire);
-      const uint32_t thread_index = g_profiler->profile_data.num_threads++;
+      const uint32_t thread_index = ft_profiler()->profile_data.num_threads++;
 
       const uint32_t k_read_offset = k_max_timestamps / 5u;
       const uint32_t k_num_read = k_max_timestamps - k_read_offset;
 
-      g_profiler->profile_data.thread_data[thread_index] = (ft_profile_data_t::ft_thread_data_t) {
-        .buffer = (uint64_t*)&g_profiler->mem_arena[timeline->buffer_address],
+      ft_profiler()->profile_data.thread_data[thread_index] = (ft_profile_data_t::ft_thread_data_t) {
+        .buffer = (uint64_t*)&ft_profiler()->mem_arena[timeline->buffer_address],
         .start = num_used > k_max_timestamps ? ((num_used + k_read_offset) % k_max_timestamps) : 0u,
         .count =  num_used > k_max_timestamps ? k_num_read : num_used,
       };
     }
   }
 
-  flush_data(&g_profiler->profile_data, user_data);
+  {
+    FT_SCOPE_TOK(flush_data_callback);
+    flush_data(&ft_profiler()->profile_data, user_data);
+  }
 
   for (uint32_t i=0u; i<k_max_threads; ++i) {
-    ft_timeline_t* timeline = &g_profiler->timeline_pool[i];
+    ft_timeline_t* timeline = &ft_profiler()->timeline_pool[i];
     if (timeline->flags_release) {
       ft_release_index(i);
       ft_reset_timeline(i);
@@ -302,29 +320,25 @@ static struct ft_profiler_i g_profiler_api = {
 };
 
 ft_profiler_i* ft_open_profiler(uint32_t num_blocks) {
-  static ft_profiler_t profiler_inst;
-  g_profiler = &profiler_inst;
+  ft_profiler()->mem_arena = (uint8_t*)malloc(num_blocks * k_num_block_bytes);
 
-  g_profiler->mem_arena = (uint8_t*)malloc(num_blocks * k_num_block_bytes);
+  memset(ft_profiler()->free_list, 0xff, sizeof(ft_profiler()->free_list));
+  memset(ft_profiler()->timeline_pool, 0x0, sizeof(ft_profiler()->timeline_pool));
+  memset(ft_profiler()->mem_arena, 0x0, sizeof(num_blocks * k_num_block_bytes));
 
-  memset(g_profiler->free_list, 0xff, sizeof(g_profiler->free_list));
-  memset(g_profiler->timeline_pool, 0x0, sizeof(g_profiler->timeline_pool));
-  memset(g_profiler->mem_arena, 0x0, sizeof(num_blocks * k_num_block_bytes));
-
-  memset(&g_profiler->profile_data, 0x0, sizeof(g_profiler->profile_data));
-  g_profiler->profile_data.thread_names = g_profiler->tokens_meta.thread_names;
-  g_profiler->profile_data.token_names = g_profiler->tokens_meta.names;
+  memset(&ft_profiler()->profile_data, 0x0, sizeof(ft_profiler()->profile_data));
+  ft_profiler()->profile_data.thread_names = ft_profiler()->tokens_meta.thread_names;
+  ft_profiler()->profile_data.token_names = ft_profiler()->tokens_meta.names;
 
   for (uint32_t i=0u; i<k_max_threads; i++) {
-    g_profiler->timeline_pool[i].buffer_address = i * k_num_block_bytes;
+    ft_profiler()->timeline_pool[i].buffer_address = i * k_num_block_bytes;
   } 
 
   return &g_profiler_api;
 }
 
 void ft_close_profiler() {
-  free(g_profiler->mem_arena);
-  g_profiler = nullptr;
+  free(ft_profiler()->mem_arena);
 }
 
 #endif // FT_PROFILER_IMPL
